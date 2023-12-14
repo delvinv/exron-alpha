eec373c0997ccb7d3663542e46adb98c0076d21d
delvinv <delvinv@users.noreply.github.com>
2023-12-14 19:41:43 +1100

    deleting unused icon files

---
+++ src/components/VolunteerCard.vue
@@ -0,0 +1,36 @@
+<script setup>
+defineProps({
+  volunteer: {
+    type: Object,
+    required: true
+  }
+})
+
+</script>
+
+<template>
+    <div class="volunteer-card">
+      <!-- Display volunteer data here -->
+      <h2>{{ volunteer.forename }} >{{ volunteer.surname }}</h2>
+      <span>Unavailable on {{ volunteer.unavailable }}</span>
+    </div>
+</template>
+
+<style scoped>
+.volunteer-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.volunteer-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+
+.volunteer-link {
+  color: darkslateblue;
+  text-decoration: none;
+}
+</style>
